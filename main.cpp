43a6afd7f65d6ce77a1ccee505b2c6d6c30e33a8
mistery576 <miguel.costa.5677@gmail.com>
2025-05-07 01:01:43 +0100

    Added some instructions

--- main.cpp
+++ main.cpp
@@ -3,10 +3,6 @@
 // CRUD : create, read, update, delete
 
 int	main(int ac, char *av[])
-BORA
-
-	lets_go("Miguel");
-	lets_go("Ze");
-
-	return 0;
-BIRL
+{
+	
+}
---
+++ src/server.cpp
@@ -0,0 +1,5 @@
+#include "../inc/server.hpp"
+
+void sartServer(){
+    
+}
