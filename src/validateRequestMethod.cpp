acde06bffcc47f4b7de8d64ac37e5a317d1b6963
Hugo Arthur Bourlot <arthur.hugo962@gmail.com>
2025-05-19 18:31:25 +0100

    ADD: parseRequest now is static function

--- src/validateRequestMethod.cpp
+++ src/validateRequestMethod.cpp
@@ -5,35 +5,34 @@
 
 namespace http {
 
-bool TcpServer::validateGet() {
-  std::cout << request.path << std::endl;
-  if (request.path == "/")
-    request.path = "login.html";
-  std::cout << request.path << std::endl;
-  std::string fullPath = "./content/" + request.path;
-  std::ifstream htmlFile(fullPath.c_str());
-  if (!htmlFile.is_open()) {
-    setResponseError("404", "Not Found");
-    return false; // TODO: throw error would be better
-  }
-  setHtmlResponse(htmlFile);
-  return (true);
-}
+	bool TcpServer::validateGet() {
+		if (request.path == "/")
+			request.path = "login.html";
+		std::cout << request.path << std::endl;
+		std::string fullPath = "./content/" + request.path;
+		std::ifstream htmlFile(fullPath.c_str());
+		if (!htmlFile.is_open()) {
+			setResponseError("404", "Not Found");
+			return false; // TODO: throw error would be better
+		}
+		setHtmlResponse(htmlFile);
+		return (true);
+	}
 
-bool TcpServer::validateRequestMethod() {
-  if (request.method == "GET") {
-    if (validateGet() == false) {
-      return (false);
-    }
-  } else if (request.method == "POST")
-    std::cout << "Suposed to do something" << std::endl;
-  else if (request.method == "DELETE")
-    std::cout << "Suposed to do something" << std::endl;
-  else {
-    setResponseError("405", "Method Not Allowed");
-    return false;
-  }
-  return true;
-}
+	bool TcpServer::validateRequestMethod() {
+		if (request.method == "GET") {
+			if (validateGet() == false) {
+				return (false);
+			}
+		} else if (request.method == "POST")
+			std::cout << "Supposed to do something" << std::endl;
+		else if (request.method == "DELETE")
+			std::cout << "Supposed to do something" << std::endl;
+		else {
+			setResponseError("405", "Method Not Allowed");
+			return false;
+		}
+		return true;
+	}
 
 } // namespace http
