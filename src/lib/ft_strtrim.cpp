1fc5091c42d1ebbb6bfa097b6f33a0a464127f27
Jorge  Marquez <joralves@student.42lisboa.com>
2025-05-21 14:48:51 +0100

    Refactorized the login to change depending  content-Type

--- src/lib/ft_strtrim.cpp
+++ src/lib/ft_strtrim.cpp
@@ -5,7 +5,7 @@ std::string ft_strtrim(const std::string &str)
 {
     unsigned int start = 0;
     unsigned int end = str.length();
-    // std::cout << std::isspace('\r');
+  
 
     while(start < str.length() && std::isspace(str[start]))
             start++;
