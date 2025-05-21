671e1205f7f27eda1e5f1e694b49f539d3b47668
Jorge Marquez <jorgemiliano91@gmail.com>
2025-05-21 12:16:46 +0100

    Chore: clang-format the files

--- src/validatePost.cpp
+++ src/validatePost.cpp
@@ -22,28 +22,28 @@ static std::map<std::string, std::string> parseForm(const std::string &body) {
 
 bool TcpServer::validatePost() {
   if (request.path == "/login") {
-      if(request.headers["Content-Type"] == "application/x-www-form-urlencoded")
-      {
-        std::map<std::string, std::string> form = parseForm(request.body);
-        if (form["username"] == "admin" && form["password"] == "1234\n") {
-          std::ifstream htmlFile("./content/success.html");
-          if (!htmlFile.is_open()) {
-            setResponseError("404", "Not Found");
-            return false;
-          }
-      setHtmlResponse("200", "OK", htmlFile);
-      return true;
-    } else {
-      std::ifstream errorFile("./content/error_401.html");
-      if (!errorFile.is_open()) {
-        setResponseError("401", "Unauthorized");
+    if (request.headers["Content-Type"] ==
+        "application/x-www-form-urlencoded") {
+      std::map<std::string, std::string> form = parseForm(request.body);
+      if (form["username"] == "admin" && form["password"] == "1234\n") {
+        std::ifstream htmlFile("./content/success.html");
+        if (!htmlFile.is_open()) {
+          setResponseError("404", "Not Found");
+          return false;
+        }
+        setHtmlResponse("200", "OK", htmlFile);
+        return true;
+      } else {
+        std::ifstream errorFile("./content/error_401.html");
+        if (!errorFile.is_open()) {
+          setResponseError("401", "Unauthorized");
+          return false;
+        }
+        setHtmlResponse("401", "Unauthorized", errorFile);
         return false;
       }
-      setHtmlResponse("401", "Unauthorized", errorFile);
-      return false;
     }
   }
-}
 
   setResponseError("404", "Not Found");
   return false;
