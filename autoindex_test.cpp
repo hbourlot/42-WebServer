85b1edb0ea895ce78dd51bf7263dde1b794c5a27
Jorge  Marquez <joralves@student.42lisboa.com>
2025-05-29 10:52:32 +0100

    Trying to implement the autoindex

--- autoindex_test.cpp
+++ autoindex_test.cpp
@@ -1,3 +1,27 @@
 // #include "http_tcpServer/http_tcpServer_linux.hpp"
+#include <dirent.h>
+#include <cstring>
+#include <iostream>
+// #include <ifstream>
+#include <fstream>
 
-int main() {}
+int main() {
+    std::string autoindex;
+    DIR *directory;
+    directory=opendir("./var/www/html");
+    struct dirent* dirent;
+    dirent = readdir(directory);
+    while(dirent != NULL)
+    {
+        std::cout << dirent->d_name << " " <<dirent->d_off << std::endl;
+        std::string d_name(dirent->d_name);
+        if(d_name.compare(".") && d_name.compare(".."))
+        autoindex += "<a href=" + d_name+ ">" +d_name + "</a>\n";
+        dirent = readdir(directory);
+    }
+    std::cout << autoindex << std::endl;
+    closedir(directory);
+    // std::ofstream autoindexfile("./var/www/html/autoindex.html");
+    // autoindexfile << autoindex ; 
+
+}
