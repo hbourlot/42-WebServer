e3706528a57aadba9a2cf87d841e8420e29d7d47
Hugo Arthur Bourlot <arthur.hugo962@gmail.com>
2025-05-31 16:34:01 +0100

    ADD: pull origin main

--- src/http_tcpServer/request/validateRequest.cpp
+++ src/http_tcpServer/request/validateRequest.cpp
@@ -4,62 +4,64 @@
 
 namespace http {
 
-const Location *getMatchLocation(const std::string &path,
-                                 const std::vector<Location> &locations) {
+	const Location *getMatchLocation(const std::string &path,
+									 const std::vector<Location> &locations) {
 
-  const Location *matchedLocation = NULL;
-  size_t matchLength = 0;
+		const Location *matchedLocation = NULL;
+		size_t matchLength = 0;
 
-  for (size_t i = 0; i < locations.size(); ++i) {
+		for (size_t i = 0; i < locations.size(); ++i) {
 
-    const std::string &locPath = locations[i].path;
+			const std::string &locPath = locations[i].path;
 
-    if (path.compare(0, locPath.size(), locPath) == 0 &&
-        locPath.size() > matchLength) {
-      matchedLocation = &locations[i];
-      matchLength = locPath.size();
-    }
-  }
-  return (matchedLocation);
-}
+			if (path.compare(0, locPath.size(), locPath) == 0 &&
+				locPath.size() > matchLength) {
+				matchedLocation = &locations[i];
+				matchLength = locPath.size();
+			}
+		}
+		return (matchedLocation);
+	}
 
-static bool validateRequestMethod(httpRequest request,
-                                  const Location *location) {
+	static bool validateRequestMethod(httpRequest request,
+									  const Location *location) {
 
-  if (request.method != "GET" && request.method != "POST" &&
-      request.method != "DELETE") {
-    return false;
-  }
+		if (request.method != "GET" && request.method != "POST" &&
+			request.method != "DELETE") {
+			return false;
+		}
 
-  for (size_t i = 0; i < location->methods.size(); ++i) {
-    if (request.method == location->methods[i])
-      return true;
-  }
-  return false;
-}
+		for (size_t i = 0; i < location->methods.size(); ++i) {
+			if (request.method == location->methods[i])
+				return true;
+		}
+		return false;
+	}
 
-bool TcpServer::validateRequest() {
+	bool TcpServer::validateRequest() {
 
-  const Location *matchedLocation =
-      getMatchLocation(request.path, infos.locations);
+		const Location *matchedLocation =
+			getMatchLocation(request.path, infos.locations);
 
-  if (!matchedLocation) {
-    setHtmlResponse("404", "Not Found", infos.errorPage[404]);
-    return false;
-  }
-  //! Treat before the /redirect-me
-  if (validateRequestMethod(request, matchedLocation) == false) {
-    setHtmlResponse("405", "Method Not Allowed", DFL_405);
-    return (false);
-  }
+		if (!matchedLocation) {
+			setHtmlResponse("404", "Not Found", infos.errorPage[404]);
+			return false;
+		}
+		//! Treat before the /redirect-me
+		if (validateRequestMethod(request, matchedLocation) == false) {
+			setHtmlResponse("405", "Method Not Allowed", DFL_405);
+			return (false);
+		}
+		// !!! HERE
 
-  if (request.method == "GET")
-    return (handleGetRequest(matchedLocation));
-  else if (request.method == "POST")
-    return (handlePostRequest(matchedLocation));
-  else if (request.method == "DELETE") {
-    // Here still mising implement the part of Method Delete
-  }
-  return (true);
-}
+		std::cout << "REQUESTTTT => " << request.path << std::endl;
+		if (request.method == "GET")
+			return (handleGetRequest(matchedLocation));
+		else if (request.method == "POST")
+			return (handlePostRequest(matchedLocation));
+		else if (request.method == "DELETE") {
+			// Here still mising implement the part of Method Delete
+		}
+		return (true);
+	}
 } // namespace http
