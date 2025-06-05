e45e07dc73073636861b2b8b7d817e22a7669f8d
Jorge Marquez <jorgemiliano91@gmail.com>
2025-06-05 10:02:16 +0100

    Using the new .clang-format file v.14

--- src/http_tcpServer/methods/handlePostRequest.cpp
+++ src/http_tcpServer/methods/handlePostRequest.cpp
@@ -1,29 +1,40 @@
 #include "http_tcpServer/Http_tcpServer_linux.hpp"
 
-namespace http {
+namespace http
+{
 
-bool TcpServer::handlePostRequest(const Location *location) {
+	bool TcpServer::handlePostRequest(const Location *location)
+	{
 
-  if (request.path == "/login") {
-    httpResponse result = validateForm(request);
+		if (request.path == "/login")
+		{
+			httpResponse result = validateForm(request);
 
-    if (!result.htmlFilePath.empty()) {
-      setHtmlResponse(result.statusCode, result.statusMessage,
-                      result.htmlFilePath);
-    } else
-      setResponseError(result.statusCode, result.statusMessage);
-  } else if (location->uploadEnable) {
-    parseMultipart(location);
-  } else if (!location->uploadEnable) {
-     {
-      setResponseError("403", "Upload Not Allowed");
-      return (false);
-    }
-  } else {
-    setHtmlResponse("404", "Not Found", infos.errorPage[404]);
-    return (false);
-  }
-  return true;
-}
+			if (!result.htmlFilePath.empty())
+			{
+				setHtmlResponse(result.statusCode, result.statusMessage,
+				                result.htmlFilePath);
+			}
+			else
+				setResponseError(result.statusCode, result.statusMessage);
+		}
+		else if (location->uploadEnable)
+		{
+			parseMultipart(location);
+		}
+		else if (!location->uploadEnable)
+		{
+			{
+				setResponseError("403", "Upload Not Allowed");
+				return (false);
+			}
+		}
+		else
+		{
+			setHtmlResponse("404", "Not Found", infos.errorPage[404]);
+			return (false);
+		}
+		return true;
+	}
 
 } // namespace http
--- src/http_tcpServer/methods/uploadHandler.cpp
+++ src/http_tcpServer/methods/uploadHandler.cpp
@@ -1,123 +1,140 @@
 #include "http_tcpServer/Http_tcpServer_linux.hpp"
-namespace http {
+namespace http
+{
 
-static std::string extractBoundary(httpRequest &request) {
+	static std::string extractBoundary(httpRequest &request)
+	{
 
-  std::string contentType = request.headers["Content-Type"];
-  std::string boundaryPrefix = "boundary=";
+		std::string contentType = request.headers["Content-Type"];
+		std::string boundaryPrefix = "boundary=";
 
-  size_t pos = contentType.find(boundaryPrefix);
-  if (pos == std::string::npos) {
-    return ("");
-  }
-  std::string boundary = "--" + contentType.substr(pos + boundaryPrefix.size());
-  return (boundary);
-}
+		size_t pos = contentType.find(boundaryPrefix);
+		if (pos == std::string::npos)
+		{
+			return ("");
+		}
+		std::string boundary =
+		    "--" + contentType.substr(pos + boundaryPrefix.size());
+		return (boundary);
+	}
 
-static std::string extractFilePart(httpRequest &request,
-                                   const std::string &boundary) {
+	static std::string extractFilePart(httpRequest &request,
+	                                   const std::string &boundary)
+	{
 
-  std::string body = request.body;
-  size_t start = body.find(boundary);
+		std::string body = request.body;
+		size_t start = body.find(boundary);
 
-  if (start == std::string::npos)
-    return ("");
+		if (start == std::string::npos)
+			return ("");
 
-  start += boundary.length() + 2; // To skip the begin boundary and the /r/n
-  size_t end = body.find(boundary, start);
+		start +=
+		    boundary.length() + 2; // To skip the begin boundary and the /r/n
+		size_t end = body.find(boundary, start);
 
-  if (end == std::string::npos)
-    end = body.size();
+		if (end == std::string::npos)
+			end = body.size();
 
-  return (body.substr(start, end - start));
-}
+		return (body.substr(start, end - start));
+	}
 
-static bool splitHeadersAndContent(const std::string &filePart,
-                                   std::string &headers, std::string &content) {
+	static bool splitHeadersAndContent(const std::string &filePart,
+	                                   std::string &headers,
+	                                   std::string &content)
+	{
 
-  size_t headerEnd = filePart.find("\r\n\r\n");
+		size_t headerEnd = filePart.find("\r\n\r\n");
 
-  if (headerEnd == std::string::npos)
-    return (false);
+		if (headerEnd == std::string::npos)
+			return (false);
 
-  headers = filePart.substr(0, headerEnd);
-  content = filePart.substr(headerEnd + 4);
+		headers = filePart.substr(0, headerEnd);
+		content = filePart.substr(headerEnd + 4);
 
-  return (true);
-}
+		return (true);
+	}
 
-static std::string extractFilename(const std::string &headers) {
+	static std::string extractFilename(const std::string &headers)
+	{
 
-  std::string token = "filename=\"";
-  size_t start = headers.find(token);
+		std::string token = "filename=\"";
+		size_t start = headers.find(token);
 
-  if (start == std::string::npos)
-    return ("");
+		if (start == std::string::npos)
+			return ("");
 
-  start += token.length();
-  size_t end = headers.find("\"", start);
+		start += token.length();
+		size_t end = headers.find("\"", start);
 
-  if (end == std::string::npos)
-    return ("");
+		if (end == std::string::npos)
+			return ("");
 
-  return (headers.substr(start, end - start));
-}
+		return (headers.substr(start, end - start));
+	}
 
-static bool saveFile(const std::string &filename, const std::string &content,
-                     const Location *location) {
+	static bool saveFile(const std::string &filename,
+	                     const std::string &content, const Location *location)
+	{
 
-  std::string savePath = location->uploadStore + '/' + filename;
+		std::string savePath = location->uploadStore + '/' + filename;
 
-  std::ofstream newfile(savePath.c_str(), std::ios::binary);
-  if (!newfile.is_open())
-    return (false);
+		std::ofstream newfile(savePath.c_str(), std::ios::binary);
+		if (!newfile.is_open())
+			return (false);
 
-  newfile << content;
-  newfile.close();
-  return (true);
-}
+		newfile << content;
+		newfile.close();
+		return (true);
+	}
 
-bool TcpServer::parseMultipart(const Location *location) {
+	bool TcpServer::parseMultipart(const Location *location)
+	{
 
-  std::string boundary = extractBoundary(request);
+		std::string boundary = extractBoundary(request);
 
-  if (boundary.empty()) {
-    setResponseError("400", "Bad Request: No boundary");
-    return (false);
-  }
+		if (boundary.empty())
+		{
+			setResponseError("400", "Bad Request: No boundary");
+			return (false);
+		}
 
-  std::string filePart = extractFilePart(request, boundary);
+		std::string filePart = extractFilePart(request, boundary);
 
-  if (filePart.empty()) {
-    setResponseError("400", "Bad Request: No boundary filePart");
-    return (false);
-  }
+		if (filePart.empty())
+		{
+			setResponseError("400", "Bad Request: No boundary filePart");
+			return (false);
+		}
 
-  std::string headers;
-  std::string content;
+		std::string headers;
+		std::string content;
 
-  if (!splitHeadersAndContent(filePart, headers, content)) {
-    setResponseError("400", "Bad Request: Malformed multipart body");
-    return (false);
-  }
+		if (!splitHeadersAndContent(filePart, headers, content))
+		{
+			setResponseError("400", "Bad Request: Malformed multipart body");
+			return (false);
+		}
 
-  std::string filename = extractFilename(headers);
+		std::string filename = extractFilename(headers);
 
-  if (filename.empty()) {
-    setResponseError("400", "Bad Request: Filename not found");
-    return (false);
-  }
+		if (filename.empty())
+		{
+			setResponseError("400", "Bad Request: Filename not found");
+			return (false);
+		}
 
-  if (!saveFile(filename, content, location)) {
-    // setResponseError("500", "Internal Server Error: File not saved");
-    setHtmlResponse("500", "Internal Server Error: File not saved", DFL_500);
-    return (false);
-  }
-  std::string msg = "File '" + filename + "' received";
+		if (!saveFile(filename, content, location))
+		{
+			// setResponseError("500", "Internal Server Error: File not saved");
+			setHtmlResponse("500", "Internal Server Error: File not saved",
+			                DFL_500);
+			return (false);
+		}
+		std::string msg = "File '" + filename + "' received";
 
-  setResponse("200", "OK", "text/plain", msg);
+		setResponse("200", "OK", "text/plain", msg);
 
-  return (true);
-}
+		return (true);
+	}
 
 } // namespace http
