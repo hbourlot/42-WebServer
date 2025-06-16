fd17137969408702453e09a5fbe5a03bc1858439
Jorge Marquez <jorgemiliano91@gmail.com>
2025-06-16 23:55:30 +0100

    Fix the upload part

--- src/http_tcpServer/clearResponse.cpp
+++ src/http_tcpServer/clearResponse.cpp
@@ -2,9 +2,20 @@
 #include "http_tcpServer/Http_tcpServer_linux.hpp"
 
 void http::TcpServer::clearResponse(httpRequest &request,
-                                    std::string &serverMessage) {
+                                    std::string &serverMessage)
+{
+	std::cerr << "Enters Here" << __func__ << std::endl;
+
 	request.headers.clear();
 	request.method.clear();
 	request.body.clear();
+
+	_response.statusCode.clear();
+	_response.statusMsg.clear();
+	_response.headers.clear();
+	_response.body.clear();
+
+	std::cout << "Clear body" << _response.body << std::endl;
+
 	m_serverMessage.clear();
 }
--- src/http_tcpServer/request/readRequest.cpp
+++ src/http_tcpServer/request/readRequest.cpp
@@ -4,33 +4,44 @@
 
 void http::TcpServer::readRequest(std::vector<pollfd> &fds, int i)
 {
+	static std::map<int, std::string> buffers; // buffer per fd
 	char buffer[BUFFER_SIZE + 1] = {0};
-	std::string requestContent;
-	while ((_bytesReceived = read(fds[i].fd, buffer, BUFFER_SIZE)) > 0)
-		requestContent.append(buffer, _bytesReceived);
-
-	if (_bytesReceived < 0)
+	ssize_t bytesReceived = read(fds[i].fd, buffer, BUFFER_SIZE);
+	if (bytesReceived <= 0)
 	{
-		if (errno != EAGAIN && errno != EWOULDBLOCK)
+		if (bytesReceived < 0 && errno != EAGAIN && errno != EWOULDBLOCK)
 		{
-
 			std::cerr << "Error: read()\n";
 			close(fds[i].fd);
 			fds.erase(fds.begin() + i);
-			return;
 		}
+		return;
 	}
-
-	// std::cout << "ON HERE\n";
-	if (buffer[0])
-		std::cout << buffer << std::endl;
-	// write(STDOUT_FILENO, buffer, bytesReceived);
-	// throw TcpServerException(
-	// 	"Failed to read bytes from client socket connection");
-	if (!requestContent.empty())
+	buffers[fds[i].fd].append(buffer, bytesReceived);
+	// Check if we have all headers
+	size_t headerEnd = buffers[fds[i].fd].find("\r\n\r\n");
+	if (headerEnd == std::string::npos)
+		return; // Wait for more data
+	// Parse headers to get Content-Length
+	std::string headers = buffers[fds[i].fd].substr(0, headerEnd + 4);
+	size_t contentLength = 0;
+	size_t pos = headers.find("Content-Length:");
+	if (pos != std::string::npos)
 	{
-		parseRequest(_request, requestContent);
-		// Set event POLLOUT
-		fds[i].events |= POLLOUT;
+		pos += 15;
+		while (pos < headers.size() && isspace(headers[pos]))
+			++pos;
+		size_t end = headers.find("\r\n", pos);
+		contentLength =
+		    std::strtoul(headers.substr(pos, end - pos).c_str(), NULL, 10);
 	}
+	// Check if we have the full body
+	size_t totalExpected = headerEnd + 4 + contentLength;
+	if (buffers[fds[i].fd].size() < totalExpected)
+		return; // Wait for more data
+	// Now we have the full request
+	std::string requestContent = buffers[fds[i].fd].substr(0, totalExpected);
+	parseRequest(_request, requestContent);
+	fds[i].events |= POLLOUT;
+	buffers.erase(fds[i].fd); // Clean up
 }
