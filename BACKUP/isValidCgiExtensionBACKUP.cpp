ecf61b6338639e03cdd908de413861b40bf52cc7
Hugo Arthur Bourlot <arthur.hugo962@gmail.com>
2025-10-14 15:01:50 +0100

    Removed folder BACKUP

--- BACKUP/isValidCgiExtensionBACKUP.cpp
+++
@@ -1,9 +0,0 @@
-#include "http_tcpServer/Cgi.hpp"
-#include "http_tcpServer/Http_tcpServer_linux.hpp"
-
-const std::set<std::string> http::Cgi::validCgiExtensions =
-    createValidCgiExtensions();
-
-bool http::Cgi::isValidCgiExtension(const std::string &ext) {
-	return validCgiExtensions.count(ext) > 0;
-};
