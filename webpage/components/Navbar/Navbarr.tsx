0b1d92dec52a739a466333f481009f1907e82713
Hugo Arthur Bourlot <arthur.hugo962@gmail.com>
2025-10-14 14:51:55 +0100

    Changes: includes in webServer and update main pulling Jorge's branch

---
+++ webpage/components/Navbar/Navbarr.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export function Navbarr() {
+	return (
+		<>
+			<nav>
+				<a href="/">Home</a>
+				<a href="services.html">Services</a>
+				<a href="login.html">Login</a>
+			</nav>
+		</>
+	);
+}
--- webpage/js/layout.js
+++ webpage/js/layout.js
@@ -1,10 +1,11 @@
-fetch("../components/navbar.html")
-  .then((res) => res.text())
-  .then((html) => {
-    document.getElementById("navbar").innerHTML = html;
-  });
-fetch("../components/footer.html")
-  .then((res) => res.text())
-  .then((html) => {
-    document.getElementById("footer").innerHTML = html;
-  });
+fetch("../../../components/Navbar/Navbar.html")
+	.then((res) => res.text())
+	.then((html) => {
+		document.getElementById("navbar").innerHTML = html;
+	});
+
+fetch("../../components/Footer/Footer.html")
+	.then((res) => res.text())
+	.then((html) => {
+		document.getElementById("footer").innerHTML = html;
+	});
